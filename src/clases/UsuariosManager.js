b240cc2340112a5c85cdc98cb544c480b27a6a83
marianorigo18 <marianowagnerrigo@gmail.com>
2023-05-17 23:40:40 -0300

    fix commit

--- src/clases/UsuariosManager.js
+++ src/clases/UsuariosManager.js
@@ -20,4 +20,19 @@ export default class UsuariosManager{
             info.id = users[users.length-1].id + 1
         }
     }
+    borrarUsuario = async (id) => {
+        const usuarios = await this.consultarUsuarios()
+        const usuariosFiltrados = usuarios.filter((usurio) => {
+            return usuarios.id != id
+        })
+        await fs.promises.writeFile(path, JSON.stringify(usuariosFiltrados), null, '\t')
+    }
+
+    consultarUsuariosPorId = async (id) => {
+        const usuarios = await this.consultarUsuarios()
+        const usuariosFiltrados = usuarios.find((usuario) => {
+            return usuario.id == id
+        })
+        return usuariosFiltrados
+    }
 }
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+import UsuariosManager from './clases/UsuariosManager.js'
+
+const app = express();
+
+const usuariosManager = new UsuariosManager()
+
+
+app.get("/usuarios", async (req, res) => {
+    const usuarios = await usuariosManager.consultarUsuarios()
+    res.send(usuarios)
+})
+
+app.get("/usuarios/:id", async(req, res) => {
+    const usuario = await usuariosManager.consultarUsuariosPorId(req.params.id)
+    res.send(usuario)
+})
+
+app.listen(8080, ()=>{
+    console.log("escuchando")
+})
